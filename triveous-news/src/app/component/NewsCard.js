0b56aecb88b1c5c3d52ad2a3eb3bd02716f4954a
Shubhdeep das <90149745+Shubhdeepdsa@users.noreply.github.com>
2023-08-06 14:51:17 +0000

    card edit

--- triveous-news/src/app/component/NewsCard.js
+++ triveous-news/src/app/component/NewsCard.js
@@ -2,10 +2,20 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import React from 'react'
 import { useUserAuth } from '../context/AuthContext'
-import makeStyles from '@mui/material';
+import styled from '@emotion/styled';
 import { Card, CardContent, Typography, CardMedia } from '@mui/material';
-
+const GlassBox = styled(Box)(({ theme }) => ({
+    backdropFilter: 'blur(10px)',
+    backgroundColor: 'rgba(212,194,252, 0.3)', // Adjust the alpha value for transparency
+    padding: '10px',
+    borderRadius: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
 const NewsCard = () => {
+
     const { news } = useUserAuth()
     return (
         <>
@@ -14,7 +24,16 @@ const NewsCard = () => {
                     { console.log(ele.title) }
                     return (
                         <Grid item xs={6}>
-                            <Box sx={{ p: 2, backgroundColor: '#D4C2FC' }}>
+                            <Box sx={{
+                                p: 2, backdropFilter: 'blur(10px)',
+                                backgroundColor: 'rgba(212,194,252, 0.3)',
+                                padding: '10px',
+                                borderRadius: '5px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}>
                                 <Card>
                                     <CardContent>
                                         <Typography variant="h6">
@@ -44,4 +63,4 @@ const NewsCard = () => {
 }
 
 
-export default NewsCard
+export default NewsCard;

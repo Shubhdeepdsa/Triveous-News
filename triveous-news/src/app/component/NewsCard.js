5cce351f738b4e0c1f96c4a1938c8b41176d4649
Shubhdeep das <90149745+Shubhdeepdsa@users.noreply.github.com>
2023-08-07 07:09:43 +0000

    major ui update

--- triveous-news/src/app/component/NewsCard.js
+++ triveous-news/src/app/component/NewsCard.js
@@ -1,66 +1,85 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import React from 'react'
-import { useUserAuth } from '../context/AuthContext'
-import styled from '@emotion/styled';
-import { Card, CardContent, Typography, CardMedia } from '@mui/material';
-const GlassBox = styled(Box)(({ theme }) => ({
-    backdropFilter: 'blur(10px)',
-    backgroundColor: 'rgba(212,194,252, 0.3)', // Adjust the alpha value for transparency
-    padding: '10px',
-    borderRadius: '5px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
+import React from 'react';
+import { useUserAuth } from '../context/AuthContext';
+import { Card, CardContent, Typography, CardMedia, IconButton } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 const NewsCard = () => {
+  const { news } = useUserAuth();
 
-    const { news } = useUserAuth()
-    return (
-        <>
-            {
-                news.map((ele) => {
-                    { console.log(ele.title) }
-                    return (
-                        <Grid item xs={6}>
-                            <Box sx={{
-                                p: 2, backdropFilter: 'blur(10px)',
-                                backgroundColor: 'rgba(212,194,252, 0.3)',
-                                padding: '10px',
-                                borderRadius: '5px',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                            }}>
-                                <Card>
-                                    <CardContent>
-                                        <Typography variant="h6">
-                                            {ele.name}
-                                        </Typography>
-                                        <Typography variant="h5">
-                                            {ele.title}
-                                        </Typography>
-                                        <Typography variant="body1">
-                                            {ele.description}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardMedia
-                                        component="img"
-                                        image={ele.urlToImage}
-                                        alt={ele.title}
-                                    />
-                                </Card>
-                            </Box>
-                        </Grid>
-                    )
-                })
-            }
-        </>
-    )
-
-}
-
+  return (
+    <>
+      {news.map((ele, index) => {
+        return (
+          <Grid key={index} item xs={12} md={6}>
+            <Box
+              sx={{
+                p: 2,
+                backdropFilter: 'blur(10px)',
+                backgroundColor: 'rgba(212, 194, 252, 0.1)',
+                borderRadius: '5px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100%',
+                transition: 'transform 0.2s',
+                '&:hover': {
+                  transform: 'scale(1.05)',
+                },
+              }}
+            >
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: '100%',
+                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                  position: 'relative', // Add relative position for positioning the like button
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  image={ele.urlToImage}
+                  alt={ele.title}
+                  sx={{
+                    height: 200,
+                    objectFit: 'cover',
+                    borderBottomLeftRadius: 0,
+                    borderBottomRightRadius: 0,
+                    filter: 'brightness(90%) contrast(120%)',
+                    transition: 'filter 0.2s',
+                    '&:hover': {
+                      filter: 'brightness(100%) contrast(100%)',
+                    },
+                  }}
+                />
+                <CardContent sx={{ color: 'lightgray', flexGrow: 1, padding: '10px 16px' }}>
+                  <Typography variant="h6">{ele.name}</Typography>
+                  <Typography variant="h5" sx={{ marginBottom: '8px' }}>
+                    {ele.title}
+                  </Typography>
+                  <Typography sx = {{color:'gray'}}variant="body1">{ele.description}</Typography>
+                </CardContent>
+                <IconButton
+                  aria-label="Like"
+                  sx={{
+                    position: 'absolute',
+                    bottom: '10px',
+                    right: '10px',
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                  }}
+                >
+                  <FavoriteIcon />
+                </IconButton>
+              </Card>
+            </Box>
+          </Grid>
+        );
+      })}
+    </>
+  );
+};
 
 export default NewsCard;
